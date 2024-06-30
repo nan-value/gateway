b260cbc72489a238726cd4cc11977e74b7558d3a
nan <devvvxz@gmail.com>
2024-06-30 13:31:24 -0700

    added packages to package list

--- build.sh
+++ build.sh
@@ -15,9 +15,32 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 rpm-ostree install screen
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Generally useful packages
+rpm-ostree install kate
+rpm-ostree install thunderbird
+rpm-ostree install haruna
+# rpm-ostree install sonaar
 
+# List of packages for creative work
+rpm-ostree install vlc
+rpm-ostree install krita
+rpm-ostree install gimp
+rpm-ostree install blender
+rpm-ostree install inkscape
+rpm-ostree install audacity
+rpm-ostree install obs-studio
+rpm-ostree install kdenlive
+rpm-ostree install shotcut
+rpm-ostree install darktable
+rpm-ostree install rawtherapee
+# rpm-ostree install gimp-plugin-registry
+# rpm-ostree install krita-plugin-gmic
+# rpm-ostree install blender-plugin-gltf
+# rpm-ostree install inkscape-plugin-gcode
+# rpm-ostree install audacity-plugin-lv2
+# rpm-ostree install obs-studio-plugin-v4l2sink
+# rpm-ostree install kdenlive-plugin-titler
+# rpm-ostree install shotcut-plugin-mlt
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
